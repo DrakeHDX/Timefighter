3b77fff0a70c4df6e08f28483e5318d874dd609f
somberous <somberous@gmail.com>
2023-09-15 01:42:17 -0400

    un-commented the BuildConfig.VERSION_NAME call

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -97,8 +97,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showInfo() {
-        val dialogTitle = getString(R.string.about_title)
-            //BuildConfig.VERSION_NAME)
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
         val dialogMessage = getString(R.string.about_message)
 
         val builder = AlertDialog.Builder(this)
